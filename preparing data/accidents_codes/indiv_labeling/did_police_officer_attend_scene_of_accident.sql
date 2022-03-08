40f8afe4543f30e3551b7fb787abbca13fb3dd96
HichamG12 <99366604+HichamG12@users.noreply.github.com>
2022-03-08 20:36:08 +0100

    Delete preparing data/accidents_codes/indiv_labeling directory

--- preparing data/accidents_codes/indiv_labeling/did_police_officer_attend_scene_of_accident.sql
+++
@@ -1,8 +0,0 @@
---did_police_officer_attend_scene_of_accident
-
-UPDATE UK_accidents.dbo.accidents
-SET did_police_officer_attend_scene_of_accident = CASE WHEN did_police_officer_attend_scene_of_accident = '1'THEN REPLACE(did_police_officer_attend_scene_of_accident, '1', 'Yes')
-                                                       WHEN did_police_officer_attend_scene_of_accident = '2'THEN REPLACE(did_police_officer_attend_scene_of_accident, '2', 'No')
-													   WHEN did_police_officer_attend_scene_of_accident = '3'THEN REPLACE(did_police_officer_attend_scene_of_accident, '3', 'No accident was reported')
-													   ELSE NULL
-													   END
